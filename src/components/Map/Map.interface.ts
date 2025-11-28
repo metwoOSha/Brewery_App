4d8ff3ebf1639b1a96523181c6144159ac512c66
Dmytro Dobrovolskyi <dimadobrovolski@gmail.com>
2025-11-28 06:09:32 +0200

    map

---
+++ src/components/Map/Map.interface.ts
@@ -0,0 +1,5 @@
+export interface MapProps {
+    latitude: number;
+    longitude: number;
+    zoom?: number;
+}
---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
+import 'leaflet-defaulticon-compatibility';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { MapProps } from './Map.interface';
+
+function Map({ latitude, longitude, zoom = 13 }: MapProps) {
+    const position: [number, number] = [latitude, longitude];
+    return (
+        <MapContainer center={position} zoom={zoom} style={{ height: '100vh' }}>
+            <TileLayer
+                attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position}>
+                <Popup></Popup>
+            </Marker>
+        </MapContainer>
+    );
+}
+
+export default Map;
